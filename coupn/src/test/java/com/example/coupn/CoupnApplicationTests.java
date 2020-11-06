3adeaee62bb8cae147a2f699e2b23eb65f346b51
xiao <1578442339@qq.com>
2020-11-06 20:12:16 +0800

    商城服务

---
+++ coupn/src/test/java/com/example/coupn/CoupnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
